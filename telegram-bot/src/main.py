85bb83737633bda5b6cff139b2c6fc29e9618b24
iafydsttta <17107370+iafydsttta@users.noreply.github.com>
2023-07-12 21:07:13 +0200

    Add /help handler

--- telegram-bot/src/main.py
+++ telegram-bot/src/main.py
@@ -3,7 +3,7 @@
 
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
 import os
 
@@ -15,13 +15,38 @@ logging.basicConfig(
 )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, 
+        text="This command does not do anything really. I'm a bot, please talk to me!")
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    # all lines after first will be indented
+    text =  \
+        """
+            Commands
+
+            /start :: Initialize chat with this bot
+            /help  :: Show this help message
+        """
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
+async def regular_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, 
+        text="No smart LLM responses here. All hardcoded goodness.")
+    
+    # just echo:
+    # await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(TOKEN).build()
-    
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-    
-    application.run_polling()
+    app = ApplicationBuilder().token(TOKEN).build()
 
+    start_handler = CommandHandler('start', start)
+    help_handler = CommandHandler('help', help)
+    app.add_handler(start_handler)
+    app.add_handler(help_handler)
+
+    text_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), regular_text)    
+    app.add_handler(text_handler)
+    
+    app.run_polling() 
